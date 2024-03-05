c36ae7c6950f935eb6e6174269066e346e704934
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-05 13:53:33 +0330

    --

--- src/message.tsx
+++ src/message.tsx
@@ -2,9 +2,11 @@
 
 function Message(){
     //jsx: javascript xml
-    const name ='alina';
+    const name ='aa';
     const family = 'asrzad'
+    if (name)
   return <h2>Hello {name} {family}</h2>;
+  return <h1> hello world</h1>
 
 }
 export default Message;
