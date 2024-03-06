b871ae1ae987df388000a9278106bd85ec8ce701
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-06 23:01:11 +0330

    ---

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,20 @@
+
 function ListGroup() {
+    const items = [
+        'tehran',
+        'shiraz',
+        'mashhad',
+        'tabriz'
+
+    ];
+   
   return (
+
     <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+
+   { items.map(item => <li>{item} </li>)}
     </ul>
+ 
   );
 }
 export default ListGroup;
