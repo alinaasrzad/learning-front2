1b7eb52e586b792a9567d76d4a697d51f91537eb
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-06 09:12:54 +0330

    --

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,31 +1,12 @@
-function ListGroup() {
+function ListGroup( ){
   return (
-    <div className="list-group">
-      <button
-        type="button"
-        className="list-group-item list-group-item-action active"
-        aria-current="true"
-      >
-        The current button
-      </button>
-      ;
-      <button type="button" className="list-group-item list-group-item-action">
-        A second button item
-      </button>
-      <button type="button" className="list-group-item list-group-item-action">
-        A third button item
-      </button>
-      <button type="button" className="list-group-item list-group-item-action">
-        A fourth button item
-      </button>
-      <button
-        type="button"
-        className="list-group-item list-group-item-action"
-        disabled
-      >
-        A disabled button item
-      </button>
-    </div>
+    <ul className="list-group">
+  <li className="list-group-item">An item</li>
+  <li className="list-group-item">A second item</li>
+  <li className="list-group-item">A third item</li>
+  <li className="list-group-item">A fourth item</li>
+  <li className="list-group-item">And a fifth one</li>
+</ul>
   );
 }
 export default ListGroup;
