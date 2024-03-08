7c93b332e89c7872e9ad5c53d6ef893f7f7bae0b
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-08 19:42:16 +0330

    ---

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,24 @@
 function ListGroup() {
   let items = ["tehran", "shiraz", "mashhad", "tabriz"];
-  items = [];
-//   const message = items.length === 0 ? <p>No item found</p> : null;
 
-  const getMessage = ()=>{
-  return items.length === 0 ? <p>No item found</p> : null;
-  }
+  //   const message = items.length === 0 ? <p>No item found</p> : null;
+
   return (
     <>
       <h1>List</h1>
-      {getMessage( )}
-      <ul>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("clicked")}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
-  );
+  ); 
 }
 export default ListGroup;
