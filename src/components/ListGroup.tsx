253766088089f95ea6141d93cb6f8c23ec70e962
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-09 17:07:22 +0330

    12

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,24 +1,47 @@
+// import { MouseEvent } from "react";
+
+
 function ListGroup() {
   let items = ["tehran", "shiraz", "mashhad", "tabriz"];
-   const handleClcik = (event) => console.log(event)
+ let selectedindex = 0;
+
+  //Event handler 
+//     
   //   const message = items.length === 0 ? <p>No item found</p> : null;
 
   return (
-    <>
-      <h1>List</h1>
-      {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item }
-            onClick={ }
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-    </>
-  ); 
+//     <>
+//       <h1>List</h1>
+//       {items.length === 0 && <p>No item found</p>}
+//       <ul className="list-group">
+//         {items.map((item, index) => (
+//           <li  
+//             className="list-group-item"
+//             key={item }
+//             onClick={ handleClcik}
+//           >
+//             {item}
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   ); 
+// }
+<>
+<h1>List</h1>
+{items.length === 0 && <p>No item found</p>}
+<ul className="list-group">
+  {items.map((item, index) => (
+    <li  
+      className={selectedindex === index ? 'list-group-item active ': 'list-group-item' }
+      key={item }
+      onClick={ () => {selectedindex = index;}}
+    >
+      {item}
+    </li>
+  ))}
+</ul>
+</>
+); 
 }
 export default ListGroup;
