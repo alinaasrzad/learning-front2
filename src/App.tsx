5daba02ec084d947f8a0500618617843753693d1
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-12 08:16:23 +0330

    --

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items =[ "iran" , "tabriz","ghom","siraz"]
  const [alertVisible , setAlertVisibilty] = useState(false)
 //  const [clickicon , setclickicon] = useState(true)
   return (
     <div>
+        <ListGroup items={items} heading="Cities" onSelectItem={function (item: string): void {
+        throw new Error("Function not implemented.");
+      } }/>
      {alertVisible && <Alert onClose={()=> setAlertVisibilty(false) }> 
         My alert 
         {/* <button className="btn btn-danger flex-xl-row "  onClick={() => setAlertVisibilty(false) }>
@@ -17,6 +22,7 @@ function App() {
       <Button color="primary"  onClick={() => setAlertVisibilty(true) }>
         click here 
       </Button>  
+    
     </div> 
   );
 }
