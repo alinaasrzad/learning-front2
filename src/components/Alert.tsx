551be34c6896e74b72fc4f60c67435c18ebe2e06
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-13 12:46:10 +0330

    --

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { Children, ReactNode } from "react";
 import { useState } from "react";
 interface Props {
     children : ReactNode;
@@ -8,9 +8,13 @@ const Alert = ( {children ,onClose}: Props) => {
     const [alertVisible , setAlertVisibilty] = useState(false)
   return (
    
-    <div className="alert alert-info">{children}  
-     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"  onClick= {onClose}></button>
-     </div>
+    // <div className="alert alert-danger">  <strong>Holy guacamole!</strong>
+    //  <button type="button" className="btn-close primary mx-104" data-bs-dismiss="alert" aria-label="Close"  onClick= {onClose}></button>
+    //  </div>
+     <div className="alert alert-info alert-dismissible fade show" role="alert">{children} 
+     <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+     <button type="button" className="btn-close " data-bs-dismiss="alert" aria-label="Close"  onClick= {onClose}></button>
+   </div>
   )
 } 
 
