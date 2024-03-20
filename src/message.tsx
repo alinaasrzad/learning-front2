8d88409b537fe634d4ef5012aa4e893f1a25b13c
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-20 08:12:31 +0330

    --

--- src/message.tsx
+++ src/message.tsx
@@ -1,17 +1,17 @@
 //PascalCasing
 let count = 0;
-const Message= () =>{
-    //jsx: javascript xml
-   
-   count++;
-   return <div>
-    <div> Message {count++}</div>
-    
-   </div>
+const Message = () => {
+  //jsx: javascript xml
 
-//     if (name)
-//   return <h2>Hello {name} {family}</h2>;
-//   return <h1> hello world</h1>
+  count++;
+  return (
+    <>
+      <div> Message {count}</div>
+    </>
+  );
 
-}
+  //     if (name)
+  //   return <h2>Hello {name} {family}</h2>;
+  //   return <h1> hello world</h1>
+};
 export default Message;
